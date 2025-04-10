002271d799923a983b5f0a6ba6cec40129f54472
Joaquimgarboggini <joaquim.garboggini@gmail.com>
2025-04-10 15:36:12 -0300

    tarefa pw :)

---
+++ minha-lista-tarefas/src/App.jsx
@@ -0,0 +1,16 @@
+import ListaTarefas from "./components/ListaTarefas";
+
+import './components/ListaTarefas.css'
+import './App.css';
+
+function App(){
+
+  return (
+    <>
+      <h1 id="title">gerenciador de tarefas</h1>
+      <ListaTarefas/>
+    </>
+  );
+}
+
+export default App;
